1d943334d8659767798b39a6d6fb48f053a11b93
Raghu <speedkingraghu@gmail.com>
2023-02-18 11:46:00 +0530

    User Sign Up successful

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -29,7 +29,17 @@ module.exports.create = function(req, res) {
 
     User.findOne({ email: req.body.email }, function(err, user) {
         if (err) { console.log('error in finding user in signing up'); return }
-    })
+
+        if (!user) {
+            User.create(req.body, function(err, user) {
+                if (err) { console.log('error in creating user while signing up'); return }
+
+                return res.redirect('/users/sign-in');
+            })
+        } else {
+            return res.redirect('back');
+        }
+    });
 }
 
 // sign in and create a session for the user
