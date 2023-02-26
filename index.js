05f7758923642ec42bd530c58a5e11b0bfa3177e
Raghu <speedkingraghu@gmail.com>
2023-02-26 13:55:16 +0530

    Created middleware to use passport and authenticate. Created specific controllers and routes

--- index.js
+++ index.js
@@ -21,12 +21,25 @@ app.use(expressLayouts);
 app.set('layout extractStyles', true);
 app.set('layout extractScripts', true);
 
-// use esxpress router
-app.use('/', require('./routes'));
-
 // set up the view engine
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
+app.use(session({
+    name: 'codeial',
+    // TODO change the secret before deployment in production mode
+    secret: 'Nothing',
+    saveUninitialized: false,
+    cookie: {
+        maxAge: (1000 * 60 * 100)
+    }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// use express router
+app.use('/', require('./routes'));
 
 app.listen(port, function(err) {
     if (err) {
