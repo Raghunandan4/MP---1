8d95c4de698dbf91c61bad10d4e26ab857f9f769
Raghu <speedkingraghu@gmail.com>
2023-03-19 13:10:32 +0530

    Setting up Mongo Store(few errors still persisting)

--- index.js
+++ index.js
@@ -26,6 +26,7 @@ app.set('layout extractScripts', true);
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
+// Mongo store is used to store the session cookie in the db
 app.use(session({
     name: 'codeial',
     // TODO change the secret before deployment in production mode
@@ -33,7 +34,15 @@ app.use(session({
     saveUninitialized: false,
     cookie: {
         maxAge: (1000 * 60 * 100)
-    }
+    },
+    store: new MongoStore({
+            mongooseConnection: db,
+            autoRemove: 'disabled'
+        },
+        function(err) {
+            console.log(err || 'connect-mongodb setup ok');
+        }
+    )
 }));
 
 app.use(passport.initialize());
