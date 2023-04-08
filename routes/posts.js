481f4cebaf2587e97ea4a07b3aaf779c528035d3
Raghu <speedkingraghu@gmail.com>
2023-04-08 23:28:58 +0530

    deleting a post using authorisation

--- routes/posts.js
+++ routes/posts.js
@@ -6,4 +6,6 @@ const postsController = require('../controllers/posts_controller');
 
 router.post('/create', passport.checkAuthentication, postsController.create); // so that anyone cannot post with html
 
+router.get('/destroy/:id', passport.checkAuthentication, postsController.destroy);
+
 module.exports = router;
