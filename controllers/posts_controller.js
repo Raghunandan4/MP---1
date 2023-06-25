dc0a3182ee76ef3a80dab454714f86a8c1530f24
Raghu <speedkingraghu@gmail.com>
2023-06-25 14:37:16 +0530

    Adding Flash messages to more contents

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -7,10 +7,12 @@ module.exports.create = async function(req, res) {
             content: req.body.content,
             user: req.user._id
         });
+        req.flash('success', 'Post Published!');
         return res.redirect('back');
     } catch (err) {
-        console.log('Error', err);
-        return;
+        // console.log('Error', err);
+        req.flash('error', err);
+        return res.redirect('back');
     }
 
 }
@@ -21,13 +23,16 @@ module.exports.destroy = async function(req, res) {
         if (post.user == req.user.id) {
             post.remove();
             await Comment.deleteMany({ post: req.params.id });
+            req.flash('success', 'Post and associated comments Deleted!');
             return res.redirect('back');
         } else {
+            req.flash('error', 'You cannot delete this post!');
             return res.redirect('back');
         }
     } catch (err) {
-        console.log('Error', err);
-        return;
+        // console.log('Error', err);
+        req.flash('error', err);
+        return res.redirect('back');
     }
 
 }
