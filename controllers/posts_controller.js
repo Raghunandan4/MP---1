b7b705551d79e43688574043d0025b9c552415b2
Raghu <speedkingraghu@gmail.com>
2023-06-26 20:29:14 +0530

    Sending Data converting to AJAX

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,12 +1,23 @@
 const Post = require('../models/post');
 const Comment = require('../models/comment');
+const { post } = require('../routes');
 
 module.exports.create = async function(req, res) {
     try {
-        await Post.create({
+        let post = await Post.create({
             content: req.body.content,
             user: req.user._id
         });
+
+        if (req.xhr) { // XML HTTP Request - xhr
+            return res.status(200).json({
+                data: {
+                    post: post
+                },
+                message: "Post Created!"
+            });
+        }
+
         req.flash('success', 'Post Published!');
         return res.redirect('back');
     } catch (err) {
