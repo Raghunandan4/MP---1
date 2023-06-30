066da150545762d2516d2a00544c46abdb4134b2
Raghu <speedkingraghu@gmail.com>
2023-07-01 00:34:39 +0530

    Deleting a Post via AJAX

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -18,6 +18,7 @@
                 success: function(data) {
                     let newPost = newPostDom(data.data.post);
                     $('#posts-list-container>ul').prepend(newPost);
+                    deletePost($(' .delete-post-button', newPost));
                 },
                 error: function(error) {
                     console.log(error.responseText);
@@ -31,7 +32,7 @@
         return $(`<li id="post-${post._id}">
             <p>
                     <small>
-                    <a class="delete-post-button" href="/posts/destroy/<%= post.id %>">X</a>
+                    <a class="delete-post-button" href="/posts/destroy/${ post._id }">X</a>
                 </small>
                 ${post.content}
                             <br>
@@ -52,6 +53,23 @@
                         </div>
             </div>
         </li>`)
+    }
+
+    // method to delete a post from DOM
+    let deletePost = function(deleteLink) {
+        $(deleteLink).click(function(e) {
+            e.preventDefault();
+            $.ajax({
+                type: 'get',
+                url: $(deleteLink).prop('href'),
+                success: function(data) {
+                    $(`#post-${data.data.post_id}`).remove();
+                },
+                error: function(error) {
+                    console.log(error.responseText);
+                }
+            });
+        });
     }
 
     createPost();
--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -34,6 +34,16 @@ module.exports.destroy = async function(req, res) {
         if (post.user == req.user.id) {
             post.remove();
             await Comment.deleteMany({ post: req.params.id });
+
+            if (req.xhr) {
+                return res.status(200).json({
+                    data: {
+                        post_id: req.params.id
+                    },
+                    message: "Post Deleted"
+                });
+            }
+
             req.flash('success', 'Post and associated comments Deleted!');
             return res.redirect('back');
         } else {
