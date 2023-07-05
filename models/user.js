30857ae3f7d47076731a99774a2263f03e738749
Raghu <speedkingraghu@gmail.com>
2023-07-05 20:41:10 +0530

    Configuring Multer

--- models/user.js
+++ models/user.js
@@ -17,9 +17,21 @@ const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true
+    },
+    avatar: {
+        type: String
     }
 }, {
     timestamps: true
+});
+
+let storage = multer.diskStorage({
+    destination: function(req, file, cb) { // cb - callback function
+        cb(null, path.join(__dirname, '..', AVATAR_PATH));
+    },
+    filename: function(req, file, cb) {
+        cb(null, file.fieldname + '-' + Date.now());
+    }
 });
 
 const User = mongoose.model('User', userSchema);
