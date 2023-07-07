ed83d9ecb8026281b36f8e962c1cc0220633e058
Raghu <speedkingraghu@gmail.com>
2023-07-07 14:03:42 +0530

    Showing the avatar, Connected from express.static in Index file

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -42,7 +42,7 @@ module.exports.update = async function(req, res) {
                 return res.redirect('back');
             });
 
-        } catch {
+        } catch (err) {
             req.flash('error', err);
             return res.redirect('back');
         }
